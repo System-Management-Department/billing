caeb8bcccc26dcbcab174055eb175026c818fe3d
TOKIOKA <t.tokioka@direct-holdings.co.jp>
2023-02-15 13:12:10 +0900

    マスターデータのキャッシュ追加

--- data/Model/SQLite.php
+++ data/Model/SQLite.php
@@ -3,57 +3,73 @@ namespace Model;
 
 class SQLite{
 	public $db;
-	public $tempData;
 	public static $tables = [
-		"divisions" => ["excludes" => ["created", "modified", "delete_flag"], "where" => "delete_flag=0"],
-		"teams" => ["excludes" => ["created", "modified", "delete_flag"], "where" => "delete_flag=0"],
-		"managers" => ["excludes" => ["created", "modified", "delete_flag"], "where" => "delete_flag=0"],
-		"payment_categories" => ["excludes" => ["created", "modified", "delete_flag"], "where" => "delete_flag=0"],
-		"summaries" => ["excludes" => ["created", "modified", "delete_flag"], "where" => "delete_flag=0"],
-		"clients" => ["excludes" => ["created", "modified", "delete_flag"], "where" => "delete_flag=0"],
-		"apply_clients" => ["excludes" => ["created", "modified", "delete_flag"], "where" => "delete_flag=0"],
-		"categories" => ["excludes" => ["created", "modified", "delete_flag"], "where" => "delete_flag=0"],
+		"divisions"          => ["alias" => ["created" => null, "modified" => null, "delete_flag" => null], "where" => "delete_flag=0"],
+		"teams"              => ["alias" => ["created" => null, "modified" => null, "delete_flag" => null], "where" => "delete_flag=0"],
+		"managers"           => ["alias" => ["created" => null, "modified" => null, "delete_flag" => null], "where" => "delete_flag=0"],
+		"payment_categories" => ["alias" => ["created" => null, "modified" => null, "delete_flag" => null], "where" => "delete_flag=0"],
+		"summaries"          => ["alias" => ["created" => null, "modified" => null, "delete_flag" => null], "where" => "delete_flag=0"],
+		"clients"            => ["alias" => ["created" => null, "modified" => null, "delete_flag" => null], "where" => "delete_flag=0"],
+		"apply_clients"      => ["alias" => ["created" => null, "modified" => null, "delete_flag" => null], "where" => "delete_flag=0"],
+		"categories"         => ["alias" => ["created" => null, "modified" => null, "delete_flag" => null], "where" => "delete_flag=0"],
 	];
 	
 	/**
 	 * コンストラクタ テーブル出力用の関数を登録
 	 */
-	function __construct(){
+	public function __construct(){
 		$this->db = new \SQLite3(dirname(DATA_DIR) . DIRECTORY_SEPARATOR . "cache" . DIRECTORY_SEPARATOR . "sqlite" . DIRECTORY_SEPARATOR . "master.sqlite3");
-		$this->db->createFunction("func", [$this, "func"]);
-		$this->tempData = [];
 	}
 	
 	/**
 	 * マスターの有効なデータをSQLiteにキャッシュ
 	 */
 	public function cache($db, $target){
+		$builder = new SQLiteTableBuilder($this->db);
 		if($target == "*"){
+			foreach(SQLite::$tables as $table => $options){
+				list($columns, $data) = $db->exportTable($table, $options["alias"], $options["where"]);
+				$builder->createTable($table, $columns, $data);
+			}
 		}else if(array_key_exists($target, SQLite::$tables)){
 			$table = $target;
 			$options = SQLite::$tables[$table];
-			$query = $db->select("ALL")->setTable($table)->setWhere($options["where"]);
-			$fields = $db->getColumnNames($table);
-			$exports = [];
-			$funcs = [];
-			foreach($fields as $field){
-				if(in_array($field, $options["excludes"])){
-					continue;
-				}
-				$query->addField("`{$field}`");
-				$exports[] = $field;
-				$funcs[] = sprintf("func(i, '%s') as `%s`", $field, $field);
-			}
-			$this->tempData = $query();
-			$stmt = $this->db->prepare("create table `{$table}` as with recursive t as (select 0 as i UNION ALL select i+1 from t limit :limit) select " . implode(",", $funcs) . " from t");
-			$stmt->bindParam(":limit", count($this->tempData), SQLITE3_INTEGER);
-			foreach($exports as $i => $field){
-				$stmt->bindParam(":c{$i}", $field, SQLITE3_TEXT);
-			}
-			$stmt->execute();
+			list($columns, $data) = $db->exportTable($table, $options["alias"], $options["where"]);
+			$builder->createTable($table, $columns, $data);
 		}
 	}
-	public function func($i, $c){
-		return $this->tempData[$i][$c];
+}
+class SQLiteTableBuilder{
+	private $db;
+	private $data;
+	private $tables;
+	public function __construct($db){
+		$this->db = $db;
+		$this->data = null;
+		$this->tables = [];
+		$this->db->createFunction("reference", [$this, "reference"], 2);
+		$results = $this->db->query("select name from sqlite_master where type='table'");
+		while(list($table)= $results->fetchArray()){
+			$this->tables[] = $table;
+		}
+	}
+	public function createTable($table, $columns, $data){
+		$this->data = $data;
+		$ref = [];
+		foreach($columns as $i => $field){
+			$ref[] = sprintf("reference(i, :c%d) as `%s`", $i, $field);
+		}
+		if(in_array($table, $this->tables)){
+			$this->db->exec("drop table `{$table}`");
+		}
+		$stmt = $this->db->prepare("create table `{$table}` as with recursive t as (select 0 as i UNION ALL select i+1 from t limit :limit) select " . implode(",", $ref) . " from t");
+		$stmt->bindParam(":limit", count($data), SQLITE3_INTEGER);
+		foreach($columns as $i => &$field){
+			$stmt->bindParam(":c{$i}", $field, SQLITE3_TEXT);
+		}
+		$stmt->execute();
+	}
+	public function reference($i, $c){
+		return $this->data[$i][$c];
 	}
 }
