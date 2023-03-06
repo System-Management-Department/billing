d4bb903644c35abbbb1413107e61de1438638d65
TOKIOKA <t.tokioka@direct-holdings.co.jp>
2023-03-06 13:36:13 +0900

    売上伝票出力画面調整

--- data/Controller/SalesOutputController.php
+++ data/Controller/SalesOutputController.php
@@ -20,16 +20,46 @@ class SalesOutputController extends ControllerBase{
 	}
 	
 	#[\Attribute\AcceptRole("admin", "entry")]
-	public function list(){
-		return new View();
-	}
-	
-	#[\Attribute\AcceptRole("admin", "entry")]
 	public function search(){
 		$db = Session::getDB();
 		$sdb = SQLite::cachedData();
-		list($columns, $data) = $db->exportTable("sales_slips", [], "");
-		$sdb->createTable("sales_slips", $columns, $data);
+		$query = $db->select("ALL")
+			->setTable("sales_slips");
+		$parameter = false;
+		if(!empty($_POST)){
+			if(!empty($_POST["slip_number"])){
+				$parameter = true;
+				$query->andWhere("slip_number like concat('%',?,'%')", preg_replace('/(:?[\\\\%_])/', "\\", $_POST["slip_number"]));
+			}
+			if(!empty($_POST["accounting_date"])){
+				$parameter = true;
+				$query->andWhere("accounting_date=?", $_POST["accounting_date"]);
+			}
+			if(!empty($_POST["division"])){
+				$parameter = true;
+				$query->andWhere("division=?", $_POST["division"]);
+			}
+			if(!empty($_POST["team"])){
+				$parameter = true;
+				$query->andWhere("team=?", $_POST["team"]);
+			}
+			if(!empty($_POST["manager"])){
+				$parameter = true;
+				$query->andWhere("manager=?", $_POST["manager"]);
+			}
+			if(!empty($_POST["billing_destination"])){
+				$parameter = true;
+				$query->andWhere("billing_destination=?", $_POST["billing_destination"]);
+			}
+			if(!empty($_POST["itemName"])){
+				$parameter = true;
+				$query->addTable("JSON_TABLE(detail, '\$.itemName[*]' COLUMNS(item_name TEXT PATH '\$')) AS t")
+					->setField("DISTINCT sales_slips.*")
+					->andWhere("item_name like concat('%',?,'%')", preg_replace('/(:?[\\\\%_])/', "\\", $_POST["itemName"]));
+			}
+		}
+		list($columns, $data) = $db->exportTable("sales_slips", [], "1=0 limit 0");
+		$sdb->createTable("sales_slips", $columns, $parameter ? $query() : []);
 		return new FileView($sdb->getFileName(), "application/vnd.sqlite3");
 	}
 	
